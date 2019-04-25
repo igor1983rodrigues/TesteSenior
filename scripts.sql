415adafe23625d7ac158b334a17e4c804003821d
igor.rodrigues <igor.rodrigues@amcom.com.br>
2019-04-25 18:47:09 -0300

    Create scripts.sql

---
+++ scripts.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `seniorteste`.`tbl_perfil` (`nome_perfil`, `sigla_perfil`) VALUES ('Administração', 'ADM');
+INSERT INTO `seniorteste`.`tbl_perfil` (`nome_perfil`, `sigla_perfil`) VALUES ('Almoxarife', 'ALF');
+
+INSERT INTO `seniorteste`.`tbl_usuario` (`email_usuario`, `nome_usuario`, `senha_usuario`, `id_perfil`) VALUES ('almoxarife@senior.com.br', 'Almoxarife', '1234', '2');
+INSERT INTO `seniorteste`.`tbl_usuario` (`email_usuario`, `nome_usuario`, `senha_usuario`, `id_perfil`) VALUES ('administracao@senior.com.br', 'Administração', '1234', '1');
