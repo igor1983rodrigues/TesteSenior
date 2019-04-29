a5ee0351d8e7a87a66e7404d2ceb5f0ad22c6fdf
igor.rodrigues <igor.rodrigues@amcom.com.br>
2019-04-29 12:58:47 -0300

    Página completa

---
+++ TesteSeniorSB/src/main/java/br/com/amcom/TesteSeniorSB/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.amcom.TesteSeniorSB.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(path = "/")
+	public String index() {
+		return "index";
+	}
+}
