7ea4c924f2e5fbfa9fe38dc330d04b8c5c489ae1
igor.rodrigues <igor.rodrigues@amcom.com.br>
2019-04-29 15:15:07 -0300

    Preparando para deploy

--- TesteSeniorSB/src/main/java/br/com/amcom/TesteSeniorSB/ServletInitializer.java
+++ TesteSeniorSB/src/main/java/br/com/amcom/TesteSeniorSB/ServletInitializer.java
@@ -1,4 +1,4 @@
-/*package br.com.amcom.TesteSeniorSB;
+package br.com.amcom.TesteSeniorSB;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,4 +11,3 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	}
 
 }
-*/
