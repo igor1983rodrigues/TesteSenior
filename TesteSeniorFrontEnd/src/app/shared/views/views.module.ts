4c81531e8f84fd7f806f8453bce22d6ebf4a3b94
igor1983rodrigues <igor.ti.1983@gmail.com>
2019-05-01 11:39:09 -0300

    Ajustes

--- TesteSeniorFrontEnd/src/app/shared/views/views.module.ts
+++ TesteSeniorFrontEnd/src/app/shared/views/views.module.ts
@@ -1,9 +1,10 @@
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgxCurrencyModule } from 'ngx-currency';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { SolicitacaoService } from 'src/app/services/solicitacao.service';
 import { CURRENCY_MASK_CONFIG_PTBR } from '../currency-mask-config.const';
@@ -25,6 +26,7 @@ import { AMModalModule } from '../modal/am-modal.module';
         AMModalModule,
         NgxCurrencyModule.forRoot(CURRENCY_MASK_CONFIG_PTBR),
         TooltipModule.forRoot(),
+        PaginationModule.forRoot(),
         ContainerModule
     ],
     exports: [
